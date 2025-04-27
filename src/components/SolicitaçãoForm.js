518fb188132dab84c760839a5bcd01af166d5dd0
simooooes1101 <edcarlos.scamargo@gmail.com>
2025-04-27 15:54:00 -0300

    Create SolicitaçãoForm.js

---
+++ src/components/SolicitaçãoForm.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SolicitaçãoForm() {
+  return (
+    <div>
+      <h2>Formulário de Solicitação</h2>
+      {/* Adicionar campos do formulário conforme necessário */}
+    </div>
+  );
+}
+
+export default SolicitaçãoForm;
